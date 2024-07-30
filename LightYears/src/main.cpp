c5e83386d4cc06d3d75c7095deff60435b08b3b1
Achie-Git <agamingacc2004@gmail.com>
2024-07-30 10:30:26 +0200

    TemplateSFML with window

--- LightYears/src/main.cpp
+++ LightYears/src/main.cpp
@@ -5,5 +5,17 @@ int main()
 {
 	sf::RenderWindow renderWindow{ sf::VideoMode(100,100),"My Window" };
 
+	while (renderWindow.isOpen())
+	{
+		sf::Event windowEvent;
+		while (renderWindow.pollEvent(windowEvent))
+		{
+			if (windowEvent.type == sf::Event::EventType::Closed)
+			{
+				renderWindow.close();
+			}
+		}
+	}
+
 	std::cout << "Hello World! \n";
 }
